de5e7fdb4f945983d891259b2a52200f1be521c1
Aiden <aidenpoulton@gmail.com>
2018-01-15 12:10:13 +0000

    Init with IoC

---
+++ WordChainKata.Domain/IoC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WordChainKata.Domain
+{
+    public class IoC
+    {
+
+        private static IServiceProvider _container;
+        public static IServiceProvider Container
+        {
+            get
+            {
+                if (_container == null)
+                { _container = ServiceCollection.BuildServiceProvider(); }
+                return _container;
+            }
+        }
+
+        /// <summary>
+        /// Allows for overriding services within test environment
+        /// </summary>
+        private static ServiceCollection _serviceCollection;
+        public static ServiceCollection ServiceCollection
+        {
+            get
+            {
+                if (_serviceCollection == null)
+                {
+                    _serviceCollection = BuildDefaultServiceCollection();
+                }
+                return _serviceCollection;
+            }
+        }
+
+        private static ServiceCollection BuildDefaultServiceCollection()
+        {
+            var services = new ServiceCollection();
+
+            
+
+            return services;
+        }
+    }
+}
